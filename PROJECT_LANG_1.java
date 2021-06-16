20ae989ab2572a675eb18628db6405a0e39fc5c0
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-06-15 18:37:19 -0700

    PROJECT_LANG_1.java
    The current version (version 1) of the first project language file for this project.

---
+++ PROJECT_LANG_1.java
@@ -0,0 +1,17 @@
+// Start of script
+// I decided to make Java the 1st project language file for this project (SimZonns) due to development on the Android platform, and Androids reliance on Java. Kotlin will be an alternative option.
+public class projectLanguageFileOne {
+	public static void main(String args) {
+		System.out.println("I decided to make Java the 1st project language file for this project (SimZonns) due to development on the Android platform, and Androids reliance on Java. Kotlin will be an alternative option.");
+}
+return main();
+int y = 3;
+System.out.println("Current project language file: " + int(y));
+break; } // No exit command exists
+// I am not the best with Java, so this script may not work
+/* File info
+* File version: 1 (Tuesday, June 15th 2021 at 6:37 pm)
+* File type: Java 10 Source file (*.java)
+* Line count (including blank lines and compiler line): 18
+*/
+// End of script
