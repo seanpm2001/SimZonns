068b95b79cd3d26823d7c90b8b6c79bd070ff9a7
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-06-15 18:52:54 -0700

    PROJECT_LANG_3_V1.swift
    An archive of the first version of the 3rd project language file for this project.

---
+++ PROJECT_LANG_3_V1.swift
@@ -0,0 +1,11 @@
+// Start of script
+// I decided to make Swift the 3rd project language file for this project (SimZonns) due to development on Apple platforms (iOS, iPadOS, MacOS)
+fun main() {
+NSString *str = @"Project language file 3)";
+str = [str stringByAppendingString:@" \nI decided to make Swift the 3rd project language file for this project (SimZonns) due to development on Apple platforms (iOS, iPadOS, MacOS)"];
+/* File info
+* File version: 1 (Tuesday, June 15th 2021 at 6:52 pm)
+* File type: Swift Source file (*.swift)
+* Line count (including blank lines and compiler line): 12
+*/
+// End of script
