37b6efaa2495958357588d62fd32198d5da05f5f
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-06-15 19:05:08 -0700

    PROJECT_LANG_4.py
    The current version (version 1) of the 4th project language file for this project.

---
+++ PROJECT_LANG_4.py
@@ -0,0 +1,18 @@
+# Start of script
+# I have decided to make Python 3 the 4th project language for this project (SimZonns) as it is good for application development on Linux, and is preferred for the ZSIM emulation on this project.
+''' '''
+class projectLanguageFile()
+def projectLanguageFileFourDesc():
+	print("Project Language File\n")
+	print("For: SimZonns")
+	print("I have decided to make Python 3 the 4th project language for this project (SimZonns) as it is good for application development on Linux, and is preferred for the ZSIM emulation on this project.")
+projectLanguageFileDesc()
+noMore = input("Press [ENTER] key to quit")
+print("The program should now be closed. If the program is still running, try closing the window with the close button. If this doen't work, end the task/process with a task/proces manager")
+break
+""" """
+# File info
+# File version: 1 (Tuesday, June 15th 2021 at 7:05 pm)
+# File type: Python 3 source file (*.py)
+# Line count (including blank lines and compiler line): 19
+# End of script
