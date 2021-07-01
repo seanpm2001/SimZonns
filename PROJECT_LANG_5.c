78d5a118dc5a3e4adddbfb6c7a79c97c002fc4aa
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-06-30 18:39:01 -0700

    PROJECT_LANG_5.c
    The current version (version 1) of the 5th project language file for this project.

---
+++ PROJECT_LANG_5.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+// Start of script
+// Warning: contains bad code, may not compile
+// I decided to make C the fifth project language for this project (SimZonns) as it is the best language to use for system functions, especially for this project on the parts where I can't use Java, Swift, or Kotlin.
+union projectLanguageFileFive() {
+int main(void) {
+    printf("Project language file 5: C");
+    printf("For: SimZonns");
+    printf("I decided to make C the fifth project language for this project (SimZonns) as it is the best language to use for system functions, especially for this project on the parts where I can't use Java, Swift, or Kotlin.\n");
+    break;
+  }
+}
+int loop1 = 1;
+while (loop1 == 1) {
+  return main();
+  break;
+}
+else {
+  break;
+  printf("Error. Loop has broken. Please close the program");
+}
+// File info
+// File version: 1 (Wednesday, June 30th 2021 at 6:39 pm)
+// File type: C Source file (*.c)
+// Line count (including blank lines and compiler line): 27
+// End of script
