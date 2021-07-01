2c1ac9cad796d60ade85819ada8bb545943b2a27
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-06-30 19:14:40 -0700

    PROJECT_LANG_8_V1.asm
    An archive of the first version of the 8th project language file for this project.

---
+++ PROJECT_LANG_8_V1.asm
@@ -0,0 +1,6 @@
+; I have decided to make Assembly the eighth project language for this project (SimZonns) as it is needed to emulate some hardware functions, and is going to be a somewhat common language for this project.
+
+; File info
+; File version: 1 (Wednesday, June 30th 2021 at 7:14 pm)
+; File type: Assembly source file (*.asm)
+; Line count (including blank lines and compiler line): 7
