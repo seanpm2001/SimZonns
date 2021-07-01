d2c1e7cec44edd7ba29b9f0490a5989a04c53ee9
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-06-30 18:23:06 -0700

    CONNECT.c
    The connection script for 2G antenna support.

---
+++ LegacyMode/VM/MobileData/AntennaConnect/2G/CONNECT.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <string.h>
+// Start of script
+// Antenna Connection for 2G cellular
+// I am not very good at programming in C. This is not yet functional, and there is lots of room for improvement.
+// Definitions
+union cellularAntenna2G;
+// Main
+int main(void) {
+    printf("Connecting...");
+    // SVG method (requires SVG support)
+    return io.file("/2G/2G.svg"); // Not proper syntax, but it is meant to return the 2G connection logo
+    wait 3000; // Waits 3 seconds
+    return speedT();
+    wait 5000; // Waits 5 seconds
+    return speedTest();
+    break;
+    exit;
+} // End of main method
+int speedT() {
+  // 2G connection speed info
+  printf("2G connection speed\nPhone calls and text messages enabled, web browsing supported but very slow");
+  break;
+}
+int speedTest() {
+  // Not yet available, will be a separate header script.
+  break; 
+}
+return main();
+exit;
+if (exit == false); {
+  printf("Failed to exit, the script cannot end. Please restart the SimZonns 2G Antenna virtual machine");
+  break;
+}
+/* File info
+* File version: `1 (Wednesday, June 30th 2021 at 6:23 pm)
+* File type: C Source file (*.c)
+* Line count (including blank lines and compiler line): 42
+*/
+// This script is incomplete, and needs lots of work
+// End of script
