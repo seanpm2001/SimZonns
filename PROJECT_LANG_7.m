d6988486d186a2f9c9ac70e775670f5223893a4a
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-06-30 19:10:21 -0700

    PROJECT_LANG_7.m
    The current version (version 1) of the 7th project language file for this project.

---
+++ PROJECT_LANG_7.m
@@ -0,0 +1,21 @@
+@interface classname : projectLanguageFileSeven {
+  // Start of script
+  // instance variables
+}
+  + classMethod1;
+  + (return_type)classMethod2;
+  + (return_type)classMethod3:(param1_type)param1_varName;
+  - (return_type)instanceMethod1With1Parameter:(param1_type)param1_varName;
+  - (return_type)instanceMethod2With2Parameters:(param1_type)param1_varName
+param2_callName:(param2_type)param2_varName;
+  @MacOS_10_1_ObjectiveC classname : superclassname {
+    // I chose Objective-C as the senth project language file for this project (SimZonns) as it is needed for some system level work and access on both old and new Apple mobile platforms.
+	NSLog(@"I chose Objective-C as the senth project language file for this project (SimZonns) as it is needed for some system level work and access on both old and new Apple mobile platforms.");
+}
+@end
+/* File info
+* File version: `1 (Wednesday, June 30th 2021 at 7:10 pm)
+* File type: Objective-C Source file (*.m)
+* Line count (including blank lines and compiler line): 22
+* This script is incomplete, and needs lots of work */
+// End of script
