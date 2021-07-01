6717d12b784d471fafeaa1150f02fd5491ff8f95
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-06-30 19:05:05 -0700

    PROJECT_LANG_6.sh
    The current version (version 1) of the 6th project language file for this project.

---
+++ PROJECT_LANG_6.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# Start of script
+# I have chosen Shell as the sixth project language file for this project (SimZonns) because Shell is the go-to language for debug scripting on UNIX, and is native to the UNIX desktop/mobile scheme that this project represents.
+echo (" have chosen Shell as the sixth project language file for this project (SimZonns) because Shell is the go-to language for debug scripting on UNIX, and is native to the UNIX desktop/mobile scheme that this project represents.");
+break;
+# File info
+# File version: 1 (Wednesday, June 30th 2021 at 7:05 pm)
+# File type: Bourne Again SHell Script file (*.sh)
+# Line count (including blank lines and compiler line): 11
+# End of script
