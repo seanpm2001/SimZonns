b3fe51a87992801b71d112b032658959e1ff3671
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-06-30 18:32:35 -0700

    CONNECT_V1.c
    An archive of the first version of the connection script for 5G Cellular connections.

---
+++ LegacyMode/VM/MobileData/AntennaConnect/5G/OldVersions/1/1-100/CONNECT_V1.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <string.h>
+// Start of script
+// Antenna Connection for 5G cellular
+// I am not very good at programming in C. This is not yet functional, and there is lots of room for improvement.
+// Definitions
+union cellularAntenna5G;
+// Main
+int main(void) {
+    printf("Connecting...");
+    // SVG method (requires SVG support)
+    return io.file("/5G/5G.svg"); // Not proper syntax, but it is meant to return the 5G connection logo
+    wait 3000; // Waits 3 seconds
+    return speedT();
+    wait 5000; // Waits 5 seconds
+    return speedTest();
+    break;
+    exit;
+} // End of main method
+int speedT() {
+  // 5G connection speed info
+  printf("5G connection speed\nPhone calls and text messages enabled, web browsing supported with high, multi-gigabit speed. Minimum: UNKNOWN/kb");
+  break;
+}
+int speedTest() {
+  // Not yet available, will be a separate header script.
+  break; 
+}
+return main();
+exit;
+if (exit == false); {
+  printf("Failed to exit, the script cannot end. Please restart the SimZonns 5G Antenna virtual machine");
+  break;
+}
+/* File info
+* File version: `1 (Wednesday, June 30th 2021 at 6:32 pm)
+* File type: C Source file (*.c)
+* Line count (including blank lines and compiler line): 42
+*/
+// This script is incomplete, and needs lots of work
+// End of script
